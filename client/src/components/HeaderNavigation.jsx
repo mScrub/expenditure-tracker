b27a66223d999b994520f22843366f42a985293b
Felix L. <flieu@my.bcit.ca>
2024-02-03 10:52:21 -0800

    style(HeaderNav): update HeaderNavigation styling

--- client/src/components/HeaderNavigation.jsx
+++ client/src/components/HeaderNavigation.jsx
@@ -4,21 +4,23 @@ import classes from '../components/HeaderNavigation.module.css'
 
 const Header = () => {
     return <Fragment>
-        <header className={classes.header}>
-            <nav>
-                <ul className={classes.list}>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/About'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/Tracker'>Tracker</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <div className={classes.container}>
+            <header className={classes.header}>
+                <nav>
+                    <ul className={classes.list}>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/About'>About</Link>
+                        </li>
+                        <li>
+                            <Link to='/Tracker'>Tracker</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
     </Fragment>
 }
 
