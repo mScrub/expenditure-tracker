8269450c6ff2b96c84c124d059eeb95ba60065f9
Felix L. <flieu@my.bcit.ca>
2024-03-13 20:22:12 -0700

    refactor: removed fetch req for expense post for future RTK implementation

--- server/src/utilities/server.obj.lookup.js
+++ server/src/utilities/server.obj.lookup.js
@@ -2,11 +2,8 @@ const SERVER_RTN_MSG_USER = {
     missingInputFieldsOnCreation: "Username, email, and password are needed",
     missingInputFieldsOnAuth: "Username and password are needed",
     duplicateUsername : "Duplicate username",
-}
-
-const TOKEN_EXPIRATION = {
-    expiresIn30s: "30s",
-    epxiresIn1d: "1d",
+    nonExistentUser: "User not found",
+    successfulLogin: "Successful Login"
 }
 
 const ERROR_CODE_MSG = {
